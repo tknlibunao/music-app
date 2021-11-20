aecd6a3b8fa41b623e900182d2bb372945737341
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-11-20 10:36:13 +0800

    created artists_controller.rb and implemented CRUD methods (index, new, create, edit, update, destroy, set_artist, artist_params)

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,47 @@
+class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:edit, :update, :destroy]
+  
+  def index
+    @artists = Artist.all
+  end
+  
+  def new
+    @artist = Artist.new
+  end
+  
+  def create
+    @artist = Artist.new(artist_params)
+    
+    if @artist.save
+      redirect_to artists_path, notice: 'New artist successfully created'
+    else
+      render :new, error: 'Failed to create artist'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @artist.update(artist_params)
+      redirect_to artists_path, notice: 'Artist details successfully updated'
+    else
+      render :edit, error: 'Failed to update artist details'
+    end
+  end
+  
+  def destroy
+    @artist.destroy
+    redirect_to artists_path, notice: 'Artist data was deleted'
+  end
+  
+  private
+  
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+  
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+end
