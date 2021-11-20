e4f3e09c6649300b9a37f819c687647a4453edae
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-11-21 01:55:11 +0800

    added route under resources :artists for action purge_avatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :artists
+  resources :artists do
+    member do
+      delete :purge_avatar
+    end
+  end
+  
   resources :albums
   resources :tracks
 end
