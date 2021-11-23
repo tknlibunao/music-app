441c9a738218b52bc998128a3d034b23455f321f
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2021-11-23 10:54:33 +0800

    added require statements for rendering of stylesheets and images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require('stylesheets/application.scss')
+require.context('../../assets/images', true)
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
